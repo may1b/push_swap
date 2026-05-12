15f844b74968e9f69e228f3e232009d88acbc40b
Achilles Scheufele <ascheufe@1-C-18.42heilbronn.de>
2026-05-12 10:06:14 +0200

    Made Input norminette comform

--- input/check_str.c
+++ input/check_str.c
@@ -6,18 +6,17 @@
 /*   By: ascheufe <ascheufe@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 11:42:05 by ascheufe          #+#    #+#             */
-/*   Updated: 2026/05/12 09:51:27 by ascheufe         ###   ########.fr       */
+/*   Updated: 2026/05/12 10:05:21 by ascheufe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-
-bool only_num_space(char *str)
+bool	only_num_space(char *str)
 {
 	size_t	i;
 	bool	space_before;
-	
+
 	i = 0;
 	space_before = true;
 	if (str[0] == '-' && str[1])
@@ -31,65 +30,52 @@ bool only_num_space(char *str)
 	return (true);
 }
 
-bool has_dup(t_stack numb)
+char	**norm(char *argv, t_int_arr *numb)
 {
-	size_t	i;
-	size_t	x;
-	
-	i = 0;
-	x = 0;
-	while (i < (numb.top + 1))
-	{
-		x = i + 1;
-		while (x < (numb.top + 1))
-		{
-			if (numb.arr[i] == numb.arr[x])
-				return (true);
-			x++;
-		}
-		i++;
-	}
-	return (false);
-}
-
-t_int_arr check_string_and_create(char *argv, int *alg_selected)
-{
-	t_int_arr	numb;
 	char	**arr;
-	size_t	i;
-	size_t	x;
-	static bool was_selected = false;
 
-	i = 0;
-	x = 0;
 	arr = ft_split(argv, ' ');
 	if (!arr)
 		error_fun(ENOMEM);
 	if (!ft_strchr(argv, ' '))
-		numb.ln = 1;
+		numb->ln = 1;
 	else
-		numb.ln = str_arr_len(arr);
-	numb.numbers = malloc(sizeof(int) * numb.ln);
-	if (!numb.numbers)
+		numb->ln = str_arr_len(arr);
+	numb->numbers = malloc(sizeof(int) * numb->ln);
+	if (!numb->numbers)
 		error_fun(EINVAL);
+	return (arr);
+}
+
+t_int_arr	check_string_and_create(char *argv, int *alg_selected)
+{
+	t_int_arr	numb;
+	char		**arr;
+	size_t		i;
+	size_t		x;
+	static bool	was_selected = false;
+
+	i = 0;
+	x = 0;
+	arr = norm(argv, &numb);
 	while (i < numb.ln)
 	{
 		if (!only_num_space(arr[i]))
 		{
 			*alg_selected = selected_alg(arr[i]);
-			if ((*alg_selected == ALG_NONE) || !(i == 0 || i == numb.ln - 1) || was_selected)
+			if ((*alg_selected == ALG_NONE) || !(i == 0 || i == numb.ln - 1)
+				|| was_selected)
 				error_fun(EINVAL);
 			i++;
 			was_selected = true;
-			continue;
+			continue ;
 		}
-		numb.numbers[x] = ft_atoi(arr[i]);
-		x++;
-		i++;
+		numb.numbers[x++] = ft_atoi(arr[i++]);
 	}
 	numb.ln = x;
 	return (numb);
 }
+
 int	selected_alg(char *argv)
 {
 	size_t	x;
@@ -100,7 +86,6 @@ int	selected_alg(char *argv)
 	arguments[2] = "--complex";
 	arguments[3] = "--adaptive";
 	arguments[4] = NULL;
-
 	x = 0;
 	while (arguments[x])
 	{
--- input/input.c
+++ input/input.c
@@ -6,40 +6,50 @@
 /*   By: ascheufe <ascheufe@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 09:57:40 by ascheufe          #+#    #+#             */
-/*   Updated: 2026/05/11 16:59:32 by ascheufe         ###   ########.fr       */
+/*   Updated: 2026/05/12 10:05:49 by ascheufe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-t_stack input_handler(int argc, char **argv, int *alg_selected)
+bool	has_dup(t_stack numb)
 {
-	t_stack	stack;
-	t_int_arr	num;
 	size_t	i;
 	size_t	x;
-	size_t	r;
-	static bool was_selected = false;
+
+	i = 0;
+	x = 0;
+	while (i < (numb.top + 1))
+	{
+		x = i + 1;
+		while (x < (numb.top + 1))
+		{
+			if (numb.arr[i] == numb.arr[x])
+				return (true);
+			x++;
+		}
+		i++;
+	}
+	return (false);
+}
+
+t_stack	input_handler(int argc, char **argv, int *alg_selected)
+{
+	t_stack		stack;
+	t_int_arr	num;
+	size_t		i;
+	size_t		x;
+	size_t		r;
 
 	i = 1;
 	x = 0;
 	r = 0;
 	stack.top = -1;
-
 	if (argc < 2)
 		error_fun(EINVAL);
 	while (i < argc)
 	{
-		num = check_string_and_create(argv[i], alg_selected); 
-		if(num.ln == 0)			// * Alg got selected
-		{
-			if ((i == argc - 1) && !was_selected)
-				return (stack);
-			else if (i == 1)
-				was_selected = true;
-			else
-				error_fun(EINVAL);
-		}
+		num = check_string_and_create(argv[i], alg_selected);
 		while (x < num.ln)
 		{
 			stack.arr[r++] = num.numbers[x++];
